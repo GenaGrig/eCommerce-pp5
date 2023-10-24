5f3e3373f8d70c950624f137c999364857fe712f
GenaGrig <gennadiuss@gmail.com>
2023-10-24 21:52:30 +0000

    Add: breadcrumb for product detail page

--- products/templatetags/breadcrumb.py
+++ products/templatetags/breadcrumb.py
@@ -16,3 +16,15 @@ def breadcrumb(category):
         category = category.parent_category_id
 
     return breadcrumbs
+
+
+@register.simple_tag
+def product_breadcrumb(product):
+    ''' Return the breadcrumb for the given product'''
+    category = product.category
+    breadcrumbs = breadcrumb(category)
+
+    # Add the product to the breadcrumb
+    breadcrumbs.append({'name': product.name, 'url': ''})
+
+    return breadcrumbs
