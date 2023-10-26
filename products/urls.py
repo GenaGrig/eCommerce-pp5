c7b59982daf38689e2fe296b80af2838531c8875
GenaGrig <gennadiuss@gmail.com>
2023-10-26 18:37:55 +0000

    Add: Sorting functionality by different oprtions

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.all_products, name="products"),
+    path("products/", views.all_products, name="products"),
     path("<product_id>", views.product_detail, name="product_detail"),
     path("category/<category_id>", views.products_in_category, name="products_in_category"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 
 def all_products(request):
@@ -11,16 +12,39 @@ def all_products(request):
     quantity_in_stock = Product.objects.filter(quantity_in_stock__gt=0).count()
     query = None
     category = None
-    
+    sort = None
+    direction = None
+
     if request.GET:
-           if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+        if 'q' in request.GET:
+            query = request.GET['q'].strip()  # Strip whitespace from the query
+            if not query:  # Check if the query is empty after stripping whitespace
+                messages.error(request, "You didn't enter any valid search criteria!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            elif sortkey == 'price':
+                sortkey = 'price'
+            elif sortkey == 'rating':
+                sortkey = 'rating'
+            else:
+                sortkey = 'name'  # Set a default sort field if 'sort' is not recognized
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
@@ -28,6 +52,7 @@ def all_products(request):
         'search_term': query,
         "current_categories": category,
         'show_delivery_banner': True,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
