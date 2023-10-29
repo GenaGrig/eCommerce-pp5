93184016414adf71bb241a5631581408ca62d4a8
GenaGrig <gennadiuss@gmail.com>
2023-10-29 00:45:57 +0000

    Add: starting implement coupon model and functionality

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Coupon(models.Model):
+    '''A model to represent discount coupons'''
+
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
