a02e4a32edeec79ff86fde576d2c97d9d7cb52d0
GenaGrig <gennadiuss@gmail.com>
2023-11-02 21:16:09 +0000

    Small changes in checkout model and toasts

--- checkout/models.py
+++ checkout/models.py
@@ -16,7 +16,7 @@ class Order(models.Model):
     email_address = models.EmailField(max_length=250, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     postal_code = models.CharField(max_length=20, null=True, blank=True)
     city = models.CharField(max_length=50, null=False, blank=False)
     country = CountryField(blank_label='Country *', null=False, blank=False)
