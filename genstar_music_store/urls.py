5d144c4d6e865e1ab0c8bd4a4cd90fcbca7a7f68
GenaGrig <gennadiuss@gmail.com>
2023-11-06 17:46:06 +0000

    Add: Checkout view, form and template basics

--- genstar_music_store/urls.py
+++ genstar_music_store/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("products/", include('products.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
