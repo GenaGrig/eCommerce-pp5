e3e3c9cfa0082b214cafd7f66bb9499d8f8c22da
GenaGrig <gennadiuss@gmail.com>
2023-11-14 23:09:00 +0000

    Changes to stripe webhooks, success on payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 import stripe
+import json
 
 from cart.contexts import cart_contents
 from products.models import Product
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    ''' A view to cache checkout data'''
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -73,8 +94,13 @@ def checkout(request):
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
+            order_id=request.POST.get('order_id'),
+            metadata={
+                'cart': json.dumps(request.session.get('cart', {})),
+                'username': request.user,
+            }
         )
-
+        
         order_form = OrderForm()
 
     if not stripe_public_key:
@@ -115,5 +141,5 @@ def checkout_success(request, order_id):
         'order': order,
         'save_info': save_info,
     }
-
+    
     return render(request, template, context)
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+import stripe
+from .models import Order
 
 
 class StripeWH_Handler:
@@ -19,6 +21,41 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        pid = intent.id
+        
+        try:
+            # Check if 'cart' is present in metadata
+            cart = intent.metadata.get('cart', {})
+            save_info = intent.metadata.get('save_info')
+        
+            # Get the Charge object
+            stripe_charge = stripe.Charge.retrieve(
+                intent.latest_charge
+            )
+
+            billing_details = stripe_charge.billing_details
+            shipping_details = intent.shipping
+            grand_total = round(stripe_charge.amount / 100, 2)
+            
+            order_id = self.request.POST.get('order_id')
+
+            if order_id:
+                try:
+                    order = Order.objects.get(pk=order_id)
+                    print("Order Details:")
+                    print("Order ID:", order.id)
+                    print("Grand Total:", grand_total)
+
+                except Order.DoesNotExist:
+                    print("Order not found for ID:", order_id)
+            else:
+                print("Order ID not found in metadata.")
+                
+        except KeyError as e:
+            print("KeyError:", e)
+            print('Cart data not found in metadata.')
+        
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
