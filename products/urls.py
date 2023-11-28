80c7c21c119fff0598d3ed84103b36a1f00bbc38
GenaGrig <gennadiuss@gmail.com>
2023-11-28 18:07:13 +0000

    Product Admin - Editing products functionality

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("add_to_wishlist/<product_id>", views.add_to_wishlist, name="add_to_wishlist"),
     path("remove_from_wishlist/<product_id>", views.remove_from_wishlist, name="remove_from_wishlist"),
     path("add/", views.add_product, name="add_product"),
+    path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
 ]
