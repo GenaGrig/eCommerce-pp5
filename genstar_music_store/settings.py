23a19fff4da12f313a584037c5587ff9e70b16a9
GenaGrig <gennadiuss@gmail.com>
2023-12-05 15:58:55 +0000

    Fix database configuration

--- genstar_music_store/settings.py
+++ genstar_music_store/settings.py
@@ -139,17 +139,17 @@ WSGI_APPLICATION = 'genstar_music_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 
