031e0a870eb4e8e1ce97a436ad94a3758ea67c28
GenaGrig <gennadiuss@gmail.com>
2023-12-05 21:29:21 +0000

    Fix user_profile filter in order_history view

--- profiles/views.py
+++ profiles/views.py
@@ -116,7 +116,8 @@ def orders_history(request):
 
 def order_history(request, order_id):
     '''Display the user's order history'''
-    order = Order.objects.filter(user_profile=get_user_profile(request.user)).order_by('-created')[:3]
+    
+    order = Order.objects.filter(user_profile=request.user.user_profile).order_by('-created')[:3]
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_id}. '
