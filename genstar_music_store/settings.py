8eab6c69143610b72473730f6dc04221275c704d
GenaGrig <gennadiuss@gmail.com>
2023-12-18 20:08:19 +0000

    Changed email host and password

--- genstar_music_store/settings.py
+++ genstar_music_store/settings.py
@@ -239,3 +239,4 @@ else:
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", '')
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '')
     EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER", '')
