35fa0a74ad3c0cd024e8622aab6cfce2d7f21a9b
GenaGrig <gennadiuss@gmail.com>
2023-12-18 21:29:32 +0000

    Update DEBUG setting based on environment variable

--- genstar_music_store/settings.py
+++ genstar_music_store/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     "8000-genagrig-ecommercepp5-ffpiw04d4zh.ws-eu106.gitpod.io",
