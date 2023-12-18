91a57b6f36ba3da650eda3b0a156edf7f48988c5
GenaGrig <gennadiuss@gmail.com>
2023-12-18 21:05:51 +0000

    Refactor email sending in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,23 +17,7 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
-        
-    def _send_confirmation_email(self, order):
-        """Send the user a confirmation email"""
-        cust_email = order.email_address
-        subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order})
-        body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )
+
 
     def handle_event(self, event):
         """
@@ -63,6 +47,7 @@ class StripeWH_Handler:
             grand_total = round(stripe_charge.amount / 100, 2)
             
             order = self.request.POST.get('order.id')
+            print("Order ID:", order)
 
             if order:
                 try:
@@ -76,7 +61,14 @@ class StripeWH_Handler:
         except KeyError as e:
             print("KeyError:", e)
             print('Cart data not found in metadata.')
-        self._send_confirmation_email(order)
+        send_mail(
+            subject='Genstar Music Store - Order Confirmation',
+            message=render_to_string(
+                'checkout/confirmation_emails/confirmation_email.txt',
+                {'order': order}),
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[order.email_address]
+        )
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
