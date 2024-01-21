5c6edf269d13fa38f3cd118e00457a4f1b9e525d
GenaGrig <gennadiuss@gmail.com>
2024-01-21 15:42:38 +0000

    Update store URLs

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
 from products.views import all_products
+from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("store/", views.index, name="index"),
     path('products/', all_products, name='all_products'),
     path('page-under-construction/',
          views.page_under_construction,
