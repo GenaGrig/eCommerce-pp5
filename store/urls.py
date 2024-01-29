698b6fbc725593dd80e6516882b3161e08bf38a6
GenaGrig <gennadiuss@gmail.com>
2024-01-29 20:13:03 +0000

    Update store URL pattern

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from products.views import all_products
 from . import views
 
 urlpatterns = [
-    path("store/", views.index, name="index"),
+    path("", views.index, name="index"),
     path('products/', all_products, name='all_products'),
     path('page-under-construction/',
          views.page_under_construction,
